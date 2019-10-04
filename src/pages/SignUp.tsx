5fa8b884291971941352f1ad5ae32ecc89775186
Nicolas G <nicolas.grant@gmail.com>
2019-10-04 16:32:55 -0400

    put in some crappy forms and basic routing

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,13 @@
+import { Container, Paper } from "@material-ui/core";
+
+import React from "react";
+
+export const SignUp: React.FC = () => {
+	return (
+		<Container>
+			<Paper>
+				Some form of thing here for signup.
+			</Paper>
+		</Container>
+	);
+}
